12e8eff40ce44f29d849c2e51a0049f22bb403ad
halfmoon01 <160722242+halfmoon01@users.noreply.github.com>
2024-07-09 14:59:03 +0900

    Add files via upload

---
+++ Day2.java
@@ -0,0 +1,28 @@
+package Algorithm;
+
+import java.util.Scanner;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+public class Day2 {
+	public static void main(String[] args) {
+		ArrayList<Integer> ar = new ArrayList<>();
+		ArrayDeque<Integer> qu = new ArrayDeque<>();
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int K = sc.nextInt();
+		
+		for(int i=1; i<=N; i++) {
+			qu.addLast(i);
+		}
+		System.out.print("<");
+		for(int i=0; i<N-1; i++) {
+			for(int j=0; j<K-1; j++) {
+				int M = qu.removeFirst();
+				qu.addLast(M);
+			}
+			int M = qu.removeFirst();
+			System.out.print(M+", ");
+		}
+		System.out.println(qu.removeFirst()+ ">");
+	}
+}
