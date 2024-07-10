66c44499022e5bfd1367f38dc747c5331bc8734d
halfmoon01 <160722242+halfmoon01@users.noreply.github.com>
2024-07-10 15:04:52 +0900

    Add files via upload

---
+++ Day3.java
@@ -0,0 +1,47 @@
+package Algorithm;
+
+import java.util.Queue;
+import java.util.ArrayDeque;
+import java.util.Iterator;
+class Day3 {
+	public static void main(String[] args) {
+		int[] array = new int[] {10};
+		System.out.println(solution(100, 100, array));
+	}
+	
+	public static void  Printing(Queue<Integer> prQueue) {        
+
+		      Iterator iT = prQueue.iterator();
+		      System.out.print("Contents of the queue are :");
+		      
+		      while(iT.hasNext()) {
+		         System.out.print(iT.next());  
+		      }
+		      System.out.println("\n");
+		}
+	
+    public static int solution(int bridge_length, int weight, int[] truck_weights) {
+    	Queue<Integer> bridge = new ArrayDeque<>();
+
+        int answer = 0;
+        for(int i=0; i<bridge_length; i++){
+            bridge.add(0);
+        }
+        int truckIdx = 0;
+        int trucksWeight = 0;
+        while(truckIdx < truck_weights.length){
+            answer++;
+            trucksWeight -= bridge.poll();
+            if(trucksWeight + truck_weights[truckIdx] <= weight){
+                bridge.add(truck_weights[truckIdx]);
+                trucksWeight += truck_weights[truckIdx];
+                truckIdx++;
+            }
+            else {
+                bridge.add(0);
+            }
+        }
+
+        return bridge_length + answer;
+    }
+   }
